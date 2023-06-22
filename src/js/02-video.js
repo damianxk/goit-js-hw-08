2454612dabb76c21864d6d6b4b7f3fb561b3c3e5
Damian Kowalczyk <damiank209@gmail.com>
2023-06-22 17:35:17 +0200

    added video script

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoFrame = document.getElementById('vimeo-player');
+const video = new Player(videoFrame);
+
+const savedTime = data =>
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+
+const saveTimeDelay = throttle(savedTime, 1000);
+
+const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+video.setCurrentTime(getCurrentTime).then();
+video.on('timeupdate', saveTimeDelay);
