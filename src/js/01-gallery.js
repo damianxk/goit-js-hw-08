a137d00a1332972abf32f05e1136ef9e1467e522
Damian Kowalczyk <damiank209@gmail.com>
2023-07-02 18:49:03 +0200

    updated feedback.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,8 +4,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector('.gallery');
 
 for (const item of galleryItems) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,25 @@
 import throttle from 'lodash.throttle';
 
+const storageKey = 'feedback-form-state';
+
 const form = document.querySelector('.feedback-form');
 
 const updateData = () => {
-  if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
-    form.email.value = JSON.parse(
-      localStorage.getItem('feedback-form-state')
-    ).email;
-    form.message.value = JSON.parse(
-      localStorage.getItem('feedback-form-state')
-    ).message;
+  if (JSON.parse(localStorage.getItem(storageKey))) {
+    form.email.value = JSON.parse(localStorage.getItem(storageKey)).email;
+    form.message.value = JSON.parse(localStorage.getItem(storageKey)).message;
   }
 };
 
 const inputSave = throttle(e => {
   const email = form.email.value;
   const message = form.message.value;
-  localStorage.setItem(
-    'feedback-form-state',
-    JSON.stringify({ email, message })
-  );
+  localStorage.setItem(storageKey, JSON.stringify({ email, message }));
 }, 500);
 
 const submitForm = e => {
   e.preventDefault();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(storageKey);
   form.reset();
 };
 
